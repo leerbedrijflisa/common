b642edae8aecca2d319ddea102b68985ff1a33e4
William Willing <william@williamwilling.com>
2016-03-18 09:29:58 +0100

    Patch can now receive a value of any type.

--- Lisa.Common.WebApi/Patch.cs
+++ Lisa.Common.WebApi/Patch.cs
@@ -4,6 +4,6 @@
     {
         public string Action { get; set; }
         public string Field { get; set; }
-        public string Value { get; set; }
+        public object Value { get; set; }
     }
 }
