f383eee5800242d3dd36ed47fd70ec70ca3e8fd8
William Willing <william@williamwilling.com>
2016-04-12 16:21:22 +0200

    You can now validate if a value matches a specified regex pattern.

---
+++ Lisa.Common.WebApi/Match.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Lisa.Common.WebApi
+{
+    public partial class Validator
+    {
+        protected virtual Action<string, object> Match(string pattern, RegexOptions options = RegexOptions.None)
+        {
+            return (fieldName, value) =>
+            {
+                if (value == null)
+                {
+                    return;
+                }
+
+                if (!Regex.IsMatch(value.ToString(), pattern, options))
+                {
+                    var error = Error.NoMatch(fieldName, value, pattern);
+                    Result.Errors.Add(error);
+                }
+            };
+        }
+    }
+}
